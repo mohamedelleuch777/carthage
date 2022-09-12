9aa10b47f0954c786c00b3e5d7d3e264c772cd89
mohamedelleuch777 <usher7med@gmail.com>
2022-09-12 21:24:58 +0300

    cmake evironement has been setup successfully

---
+++ CMakeFiles/carthage.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/carthage.dir/src/lexer/main.cpp.o"
+  "CMakeFiles/carthage.dir/src/lexer/main.cpp.o.d"
+  "bin/carthage"
+  "bin/carthage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/carthage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/carthage.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for carthage.
+# This may be replaced when dependencies are built.
